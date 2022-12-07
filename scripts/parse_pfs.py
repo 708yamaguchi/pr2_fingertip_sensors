183bac930f40b92b38ce707379b8fea1869b8709
Naoya Yamaguchi <708yamaguchi@gmail.com>
2022-12-07 21:41:32 +0900

    Parse sensor data from UART and USB

--- scripts/parse_pfs.py
+++ scripts/parse_pfs.py
@@ -3,7 +3,7 @@
 import rospy
 from pr2_msgs.msg import PressureState
 from pr2_fingertip_sensors.msg import PR2FingertipSensor
-from pr2_fingertip_sensors import create_pfs_msg
+from pr2_fingertip_sensors import append_packets, create_pfs_msg, int16_to_binary, parse  # NOQA
 
 
 class ParsePFS(object):
@@ -49,150 +49,41 @@ class ParsePFS(object):
         # Parse input rostopic
         for fingertip in self.fingertips:
             if fingertip == 'l_fingertip':
-                data = self.parse(msg.l_finger_tip)
+                packet_bin = int16_to_binary(msg.l_finger_tip)
+                data = parse(packet_bin)
                 frame_id = gripper + '_l_finger_tip_link'
             if fingertip == 'r_fingertip':
-                data = self.parse(msg.r_finger_tip)
+                packet_bin = int16_to_binary(msg.r_finger_tip)
+                data = parse(packet_bin)
                 frame_id = gripper + '_r_finger_tip_link'
             # Store parsed sensor data
             self.pfs_data[gripper][fingertip][data['packet_type']] = data
             # If all sensor data is stored, append them
             if self.pfs_data[gripper][fingertip][0] is not None \
                and self.pfs_data[gripper][fingertip][1] is not None:
-                header = msg.header
-                header.frame_id = frame_id
-                prox = self.pfs_data[gripper][fingertip][0]['proximity'] + \
-                    self.pfs_data[gripper][fingertip][1]['proximity']
-                prox_ordered = self.order_data(prox)
-                force = self.pfs_data[gripper][fingertip][0]['force'] + \
-                    self.pfs_data[gripper][fingertip][1]['force']
-                force_ordered = self.order_data(force)
-                acc = self.pfs_data[gripper][fingertip][0]['imu']
-                gyro = self.pfs_data[gripper][fingertip][1]['imu']
+                pfs_header = msg.header
+                pfs_header.frame_id = frame_id
+                imu_header = msg.header
+                imu_header.frame_id = '/' + gripper + '_' + fingertip + '_' + 'pfs_a_front'
+                prox_ordered, force_ordered, acc, gyro = append_packets(
+                    self.pfs_data[gripper][fingertip][0],
+                    self.pfs_data[gripper][fingertip][1])
                 self.publish(
-                    gripper, fingertip, header,
+                    gripper, fingertip, pfs_header, imu_header,
                     prox_ordered, force_ordered, acc, gyro)
                 # Reset self.pfs_data to None after publish
                 self.pfs_data[gripper][fingertip][0] = None
                 self.pfs_data[gripper][fingertip][1] = None
 
-    def publish(self, gripper, fingertip, header, proximity, force, acc, gyro):
+    def publish(
+            self, gripper, fingertip, pfs_header, imu_header,
+            proximity, force, acc, gyro):
         """
         Publish parsed sensor data
         """
         pfs_msg = create_pfs_msg(
-            gripper, fingertip, header, proximity, force, acc, gyro)
+            pfs_header, imu_header, proximity, force, acc, gyro)
         self.pub[gripper][fingertip].publish(pfs_msg)
-
-    def order_data(self, data):
-        """
-        Args
-        data: proximity or force array (length 24)
-        The order of data depends on PFS's firmware.
-        Return
-        data_ordered: proximity or force 'ordered' array (length 24)
-        The order is 'pfs_a_front', 'pfs_b_top', 'pfs_b_back', 'pfs_b_left', 'pfs_b_right'
-        """
-        data_ordered = [0] * 24
-        data_ordered[0:8] = data[0:8]
-        data_ordered[8:12] = data[16:20]
-        data_ordered[12:16] = data[20:24]
-        data_ordered[16:20] = data[8:12]
-        data_ordered[20:24] = data[12:16]
-        return data_ordered
-
-    def binary_to_int(self, binary_string):
-        """
-Args: binary string. e.g. '1010101'
-return: int value. e.g. -11
-"""
-        uint = int(binary_string, 2)
-        bits = len(binary_string)
-        if (uint & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
-            int_val = uint - (1 << bits)        # compute negative value
-        else:
-            int_val = uint
-        return int_val
-
-    def parse(self, packet):
-        """
-Args: packet is int16[22] array
-Return: dict:
-          {prox[12], force[12], imu[3](acc/gyro),
-           board_select, packet_type, check_sum}
-
-c.f.
-parse input
-$ rosmsg show pr2_msgs/PressureState
-std_msgs/Header header
-  uint32 seq
-  time stamp
-  string frame_id
-int16[] l_finger_tip
-int16[] r_finger_tip
-
-parse output
-$ rosmsg show pr2_fingertip_sensors/PR2FingertipSensor
-std_msgs/Header header
-  uint32 seq
-  time stamp
-  string frame_id
-int16[] proximity
-int16[] force
-sensor_msgs/Imu imu
-
-packet protocol
-https://docs.google.com/presentation/d/1VxRJWDqeDk_ryu-x1Vhj3_6BDu3gscwvNpngHKwfR4M/edit#slide=id.g1585f6b098c_0_0
-        """
-        # int16[] to binary
-        packet_bin = ''
-        for p in packet:
-            # Apply mask to accept negative numbers
-            packet_bin += '{0:0=16b}'.format(p & 0b1111111111111111)
-        # Proximity and Force sensor
-        prox = []
-        force = []
-        for i in range(12):
-            prox_bin = packet_bin[i*24:i*24+12]
-            prox_uint = int(prox_bin, 2)
-            prox.append(prox_uint)
-            force_bin = packet_bin[i*24+12:i*24+24]
-            force_uint = int(force_bin, 2)
-            force.append(force_uint)
-        # imu
-        imu = []
-        for i in range(3):
-            imu_bin = packet_bin[288+i*16:288+i*16+16]
-            imu_int = self.binary_to_int(imu_bin)
-            imu.append(imu_int)
-        # board_select. 0: SELECT_PFS_01_SINGLE, 1: SELECT_PFS_01_ASM
-        board_select_bin = packet_bin[336:340]
-        board_select = self.binary_to_int(board_select_bin)
-        # packet type. 0 or 1
-        packet_type_bin = packet_bin[340:344]
-        packet_type = self.binary_to_int(packet_type_bin)
-        # check_sum (Note that check_sum is uint while others are int)
-        check_sum_bin = packet_bin[344:352]
-        check_sum = int(check_sum_bin, 2)
-        # calculate check_sum in this node
-        sum_data = 0
-        for i in range(1, 42, 2):
-            packet_uint8 = int(packet_bin[i*8:i*8+8], 2)
-            sum_data += packet_uint8
-        sum_data = sum_data % 256
-        if check_sum == sum_data:
-            rospy.logdebug(
-                'check_sum ({}) is correctly calculated.'.format(check_sum))
-        else:
-            rospy.logdebug(
-                'check_sum ({}) is different from calculation ({}).'.format(
-                    check_sum, sum_data))
-        return {'proximity': prox,
-                'force': force,
-                'imu': imu,
-                'board_select': board_select,
-                'packet_type': packet_type,
-                'check_sum': check_sum}
 
 
 if __name__ == '__main__':
--- scripts/serial_test.py
+++ scripts/serial_test.py
@@ -4,68 +4,75 @@ import serial
 from serial.tools import list_ports
 import argparse
 import rospy
-import std_msgs.msg
+from std_msgs.msg import Header
 from pr2_fingertip_sensors.msg import PR2FingertipSensor
-from pr2_fingertip_sensors import create_pfs_msg
+from pr2_fingertip_sensors import append_packets, binary_to_int, create_pfs_msg, parse  # NOQA
 
 
-def publish(
-        publisher, gripper, fingertip, header, proximity, force, acc, gyro):
+class ParseSerial(object):
     """
-    Publish parsed sensor data
+    Get and parse serial data from PFS.
+    Concatenate two packets into one topic.
     """
-    pfs_msg = create_pfs_msg(
-        gripper, fingertip, header, proximity, force, acc, gyro)
-    publisher.publish(pfs_msg)
+
+    def __init__(self, serial_port, baud_rate, verbose):
+        self.pub = rospy.Publisher(
+            '/pfs/from_serial', PR2FingertipSensor, queue_size=1)
+        devices = [info.device for info in list_ports.comports()]
+        self.ser = serial.Serial(serial_port, baud_rate)
+        rospy.loginfo("current serial port list: {}".format(devices))
+        rospy.loginfo(self.ser)
+        self.packet_bytes = 44  # each packet is 44 bytes
+        self.pfs_data = [None, None]
+
+    def publish(self, header, proximity, force, acc, gyro):
+        """
+        Publish parsed sensor data
+        """
+        pfs_msg = create_pfs_msg(
+            header, header, proximity, force, acc, gyro)
+        self.pub.publish(pfs_msg)
+
+    def parse_serial(self):
+        # Get packet data. The delimiter is newline.
+        line = self.ser.readline()
+        line_disp = line.rstrip()
+        res_disp = ''.join(format(ord(i), '08b') for i in line_disp)
+        if (len(res_disp) != self.packet_bytes * 8):
+            return False
+        # Parse packet and get sensor data.
+        packet = parse(res_disp)
+        self.pfs_data[packet['packet_type']] = packet
+        # Append two packets into one PFS data
+        if self.pfs_data[0] != None and self.pfs_data[1] != None:
+            prox_ordered, force_ordered, acc, gyro = append_packets(
+                self.pfs_data[0],
+                self.pfs_data[1])
+            header = Header()
+            header.stamp = rospy.Time.now()
+            self.publish(
+                header, prox_ordered, force_ordered, acc, gyro)
+            self.pfs_data[0] = None
+            self.pfs_data[1] = None
+        return True
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--port", "-p", default="/dev/ttyUSB0", help="serial port")
-    parser.add_argument("--baud", "-b", default=115200, type=int, help="baud rate")
-    parser.add_argument("--verbose", "-v", action='store_true')
+    parser.add_argument(
+        "--port", "-p", default="/dev/ttyUSB0", help="serial port")
+    parser.add_argument(
+        "--baud", "-b", default=115200, type=int, help="baud rate")
+    parser.add_argument(
+        "--verbose", "-v", action='store_true')
     args = parser.parse_args()
     serial_port = args.port
     baud_rate = args.baud
     verbose = args.verbose
 
-    key_phrase = 'serial_publish_flatten['
-    serial_publish_length = 54
-
     rospy.init_node('serial_test')
-    publisher = rospy.Publisher('/pfs/from_serial', PR2FingertipSensor, queue_size=1)
-
-    ser = serial.Serial()
-    devices = [info.device for info in list_ports.comports()]
-    print("current serial port list: {}".format(devices))
-    ser = serial.Serial(serial_port, baud_rate)
-    print(ser)
-
-    pfs_array = [0 for _ in range(serial_publish_length)]
-    while 1:
-        line = ser.readline()
-        line_disp=line.strip().decode('UTF-8')
-        # print(line_disp)
-        if key_phrase in line_disp:
-            line_without_head = line_disp.strip(key_phrase)
-            line_num_split_str = line_without_head.split("]:")
-            line_num = [int(i) for i in line_num_split_str]
-            # print(line_num)
-            if line_num[0] == 0:
-                proximity = pfs_array[:24]
-                force = pfs_array[24:48]
-                acc = pfs_array[48:51]
-                gyro = pfs_array[51:54]
-                if verbose:
-                    print("current pfs_array: {}".format(pfs_array))
-                    print("proximity: {}".format(proximity))
-                    print("force: {}".format(force))
-                    print("acc: {}".format(acc))
-                    print("gyro: {}".format(gyro))
-                    print("-------------------------------------------------------")
-                header = std_msgs.msg.Header()
-                header.stamp = rospy.Time.now()
-                publish(publisher, 'l_gripper', 'l_fingertip',
-                        header, proximity, force, acc, gyro)
-                pfs_array = [0 for _ in range(serial_publish_length)]
-            pfs_array[line_num[0]] = line_num[1]
+    ps = ParseSerial(serial_port, baud_rate, verbose)
+    rate = rospy.Rate(100)  # 100Hz
+    while not rospy.is_shutdown():
+        ps.parse_serial()
+        rate.sleep()
